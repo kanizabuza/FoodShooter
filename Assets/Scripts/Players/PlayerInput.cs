348c9dc30c9a6eb5200d85c83b35e658bd1c5a2d
原島輝 <>
2020-10-05 22:35:49 +0900

    add editorconfig

---
+++ Assets/Scripts/Players/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Players
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
+
